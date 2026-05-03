bbb8029181abf96e8d76f6d4f6bfe76f1fab344f
Patryk Styla <43710236+obliwetator@users.noreply.github.com>
2026-05-03 23:19:50 +0200

    Add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		react(),
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler", {}]],
+			},
+		}),
 		visualizer({
 			filename: "dist/stats.html",
 			gzipSize: true,
