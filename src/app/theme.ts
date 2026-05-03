34410bfb122225f5953f49bcfca0c7121f9a7552
Patryk Styla <43710236+obliwetator@users.noreply.github.com>
2026-05-03 22:10:04 +0200

    Change font to Cascadia Code

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -2,4 +2,5 @@ import { createTheme } from "@mui/material/styles";
 
 export const darkTheme = createTheme({
 	palette: { mode: "dark" },
+	typography: { fontFamily: '"Cascadia Code", monospace' },
 });
