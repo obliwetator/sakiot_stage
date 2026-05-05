96c9d33d1860380b70c2b87419b056c7cb7d55ed
Patryk Styla <43710236+obliwetator@users.noreply.github.com>
2026-05-05 18:44:11 +0200

    Title

---
+++ src/features/audio-dashboard/RangeSlider/RangeDetails.tsx
@@ -0,0 +1,67 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import type { Dispatch, SetStateAction } from "react";
+import type { AudioParams } from "../../../Constants";
+import { formatDuration } from "../../../utils/formatTime";
+import { PlaybackSpeedSlider } from "./PlaybackSpeedSlider";
+import { TimeEditors } from "./TimeEditor";
+import { VolumeSlider } from "./VolumeSlider";
+
+export function RangeDetails(props: {
+	audioRef: HTMLAudioElement;
+	params: Readonly<Partial<Record<AudioParams, string>>>;
+	startEnd: number[];
+	setStartEnd: Dispatch<SetStateAction<number[]>>;
+	onPinEnd: () => void;
+}) {
+	return (
+		<>
+			<Stack
+				spacing={{ xs: 2, md: 8 }}
+				direction={{ xs: "column", md: "row" }}
+				alignItems={{ xs: "stretch", md: "center" }}
+				justifyContent="space-around"
+				sx={{ my: 2 }}
+			>
+				<VolumeSlider audioRef={props.audioRef} />
+				<PlaybackSpeedSlider audioRef={props.audioRef} />
+			</Stack>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: { xs: "column", md: "row" },
+					gap: 1,
+				}}
+			>
+				<Box sx={{ flex: 1, minWidth: 0 }}>
+					value 1: {formatDuration(Math.round(props.startEnd[0]))}
+				</Box>
+				<Box sx={{ flex: 1, minWidth: 0 }}>
+					Recorded in channel: {props.params.channel_id}
+					{(() => {
+						const parts = (props.params.file_name ?? "").split("-");
+						const userId = parts[1];
+						return userId ? (
+							<Box sx={{ fontSize: 12, opacity: 0.75 }}>User ID: {userId}</Box>
+						) : null;
+					})()}
+				</Box>
+				<Box>
+					<TimeEditors
+						startEnd={props.startEnd}
+						setStartEnd={props.setStartEnd}
+						audioRef={props.audioRef}
+						onPinEnd={props.onPinEnd}
+					/>
+				</Box>
+			</Box>
+			<br />
+			value 2: {formatDuration(props.startEnd[1])}
+			<br />
+			Cropped length: {formatDuration(props.startEnd[1] - props.startEnd[0])}
+			<br />
+			<br />
+			<br />
+		</>
+	);
+}
